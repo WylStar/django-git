3ec83b509555f80565d3d2059b00ed298aa977e8
wyl <376318925@qq.com>
2018-12-09 15:02:44 +0800

    天天生鲜

--- dailyfresh/df_cart/views.py
+++ dailyfresh/df_cart/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import JsonResponse
+from df_user import user_decorator
+from .models import *
 
 # Create your views here.
+@user_decorator.login
+def cart(request):
+    uid = request.session['user_id']
+    carts = CartInfo.objects.filter(user_id=uid)
+    context = {
+        'title':'购物车','uid':uid,'carts':carts
+    }
+    return render(request, 'df_cart/cart.html', context)
+
+@user_decorator.login
+def add(request,ggid,ccount):
+    uid = request.session['user_id']
+    gid = int(ggid)
+    count = int(ccount)
+    carts = CartInfo.objects.filter(user_id=uid,goods_id=gid)
+    if len(carts) >= 1:
+        cart = carts[0]
+        cart.count = cart.count+count
+    else:
+        cart = CartInfo()
+        cart.user_id = uid
+        cart.goods_id = gid
+        cart.count = count
+    cart.save()
+    if request.is_ajax():
+        count = CartInfo.objects.filter(user_id = request.session['user_id']).count()
+        return JsonResponse({'count':count})
+    else:
+        return redirect('/cart/')
+
+@user_decorator.login
+def edit(request,cart_id,count):
+    try:
+        cart = CartInfo.objects.get(pk=int(cart_id))
+        count1 = cart.count = int(count)
+        cart.save()
+        data = {'ok':0}
+    except Exception as e:
+        data = {'ok':count1}
+    return JsonResponse(data)
+
+
+@user_decorator.login
+def delete(request,cart_id):
+    try:
+        cart = CartInfo.objects.get(pk=int(cart_id))
+        cart.delete()
+        data = {'ok':1}
+    except Exception as e:
+        data = {'ok':0}
+    return JsonResponse(data)
